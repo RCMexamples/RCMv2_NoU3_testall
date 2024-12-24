be5aab1ec5174d8c663870d5e575d752875329e8
joshua-8 <joshua262144@gmail.com>
2024-12-24 00:46:49 -0800

    test on NoU3

--- RCMv2/RCMv2.ino
+++ RCMv2/RCMv2.ino
@@ -13,7 +13,7 @@ Remember to also choose the "environment" for your microcontroller in PlatformIO
 // #define RCM_HARDWARE_VERSION RCM_D1_V1 // version 1 of the RCM D1 // https://github.com/RCMgames/RCM-Hardware-D1
 // #define RCM_HARDWARE_VERSION ALFREDO_NOU2_NO_VOLTAGE_MONITOR // voltageComp will always report 10 volts https://www.alfredosys.com/products/alfredo-nou2/
 // #define RCM_HARDWARE_VERSION ALFREDO_NOU2_WITH_VOLTAGE_MONITOR // modified to add resistors VIN-30k-D36-10k-GND https://www.alfredosys.com/products/alfredo-nou2/
-// #define RCM_HARDWARE_VERSION ALFREDO_NOU3 // https://www.alfredosys.com/products/alfredo-nou3/
+#define RCM_HARDWARE_VERSION ALFREDO_NOU3 // https://www.alfredosys.com/products/alfredo-nou3/
 
 /**
 uncomment one of the following lines depending on which communication method you want to use
@@ -29,39 +29,142 @@ uncomment one of the following lines depending on which communication method you
 // See this page for information about how to set up a robot's drivetrain using the JMotor library
 // https://github.com/joshua-8/JMotor/wiki/How-to-set-up-a-drivetrain
 
+// all the servo drivers
+JMotorDriverEsp32Servo servo1Driver = JMotorDriverEsp32Servo(servo1port);
+JMotorDriverEsp32Servo servo2Driver = JMotorDriverEsp32Servo(servo2port);
+JMotorDriverEsp32Servo servo3Driver = JMotorDriverEsp32Servo(servo3port);
+JMotorDriverEsp32Servo servo4Driver = JMotorDriverEsp32Servo(servo4port);
 
+// all the motor drivers
+JMotorDriverPCA9685HBridge motor1Driver = JMotorDriverPCA9685HBridge(motor1port);
+JMotorDriverPCA9685HBridge motor2Driver = JMotorDriverPCA9685HBridge(motor2port);
+JMotorDriverPCA9685HBridge motor3Driver = JMotorDriverPCA9685HBridge(motor3port);
+JMotorDriverPCA9685HBridge motor4Driver = JMotorDriverPCA9685HBridge(motor4port);
+JMotorDriverPCA9685HBridge motor5Driver = JMotorDriverPCA9685HBridge(motor5port);
+JMotorDriverPCA9685HBridge motor6Driver = JMotorDriverPCA9685HBridge(motor6port);
+JMotorDriverPCA9685HBridge motor7Driver = JMotorDriverPCA9685HBridge(motor7port);
+JMotorDriverPCA9685HBridge motor8Driver = JMotorDriverPCA9685HBridge(motor8port);
+
+// all the encoder ports
+JEncoderQuadratureAttachInterrupt encoder2 = JEncoderQuadratureAttachInterrupt(encoder2port, 1.0, false); // multiplier, inverted
+JEncoderQuadratureAttachInterrupt encoder3 = JEncoderQuadratureAttachInterrupt(encoder3port, 1.0, false); // multiplier, inverted
+JEncoderQuadratureAttachInterrupt encoder4 = JEncoderQuadratureAttachInterrupt(encoder4port, 1.0, false); // multiplier, inverted
+JEncoderQuadratureAttachInterrupt encoder5 = JEncoderQuadratureAttachInterrupt(encoder5port, 1.0, false); // multiplier, inverted
+JEncoderQuadratureAttachInterrupt encoder6 = JEncoderQuadratureAttachInterrupt(encoder6port, 1.0, false); // multiplier, inverted
+JEncoderQuadratureAttachInterrupt encoder7 = JEncoderQuadratureAttachInterrupt(encoder7port, 1.0, false); // multiplier, inverted
+
+// variables for the drivers
+float servo1Val = 0;
+float servo2Val = 0;
+float servo3Val = 0;
+float servo4Val = 0;
+
+float motor1Val = 0;
+float motor2Val = 0;
+float motor3Val = 0;
+float motor4Val = 0;
+float motor5Val = 0;
+float motor6Val = 0;
+float motor7Val = 0;
+float motor8Val = 0;
+
+// variables for the encoders
+float encoder2Val = 0;
+float encoder3Val = 0;
+float encoder4Val = 0;
+float encoder5Val = 0;
+float encoder6Val = 0;
+float encoder7Val = 0;
 
 void Enabled()
 {
     // code to run while enabled, put your main code here
     // set all the motor drivers (you can put this in Enabled())
+    servo1Driver.set(servo1Val);
+    servo2Driver.set(servo2Val);
+    servo3Driver.set(servo3Val);
+    servo4Driver.set(servo4Val);
 
+    motor1Driver.set(motor1Val);
+    motor2Driver.set(motor2Val);
+    motor3Driver.set(motor3Val);
+    motor4Driver.set(motor4Val);
+    motor5Driver.set(motor5Val);
+    motor6Driver.set(motor6Val);
+    motor7Driver.set(motor7Val);
+    motor8Driver.set(motor8Val);
 }
 
 void Enable()
 {
     // turn on outputs
     // enable all the motor drivers (you can put this in Enable())
+    servo1Driver.enable();
+    servo2Driver.enable();
+    servo3Driver.enable();
+    servo4Driver.enable();
 
+    motor1Driver.enable();
+    motor2Driver.enable();
+    motor3Driver.enable();
+    motor4Driver.enable();
+    motor5Driver.enable();
+    motor6Driver.enable();
+    motor7Driver.enable();
+    motor8Driver.enable();
 }
 
 void Disable()
 {
     // turn off outputs
     // disable all the motor drivers (you can put this in Disable())
+    servo1Driver.disable();
+    servo2Driver.disable();
+    servo3Driver.disable();
+    servo4Driver.disable();
 
+    motor1Driver.disable();
+    motor2Driver.disable();
+    motor3Driver.disable();
+    motor4Driver.disable();
+    motor5Driver.disable();
+    motor6Driver.disable();
+    motor7Driver.disable();
+    motor8Driver.disable();
 }
+
+// create interrupt functions for the encoders (you can put this above (and outside) the PowerOn() function)
+jENCODER_MAKE_ISRS_MACRO(encoder2);
+jENCODER_MAKE_ISRS_MACRO(encoder3);
+jENCODER_MAKE_ISRS_MACRO(encoder4);
+jENCODER_MAKE_ISRS_MACRO(encoder5);
+jENCODER_MAKE_ISRS_MACRO(encoder6);
+jENCODER_MAKE_ISRS_MACRO(encoder7);
 
 void PowerOn()
 {
     // runs once on robot startup, set pin modes and use begin() if applicable here
-
+    // set up all the motor drivers (you can put this in PowerOn())
+    encoder2.setUpInterrupts(encoder2_jENCODER_ISR_A, encoder2_jENCODER_ISR_B);
+    encoder3.setUpInterrupts(encoder3_jENCODER_ISR_A, encoder3_jENCODER_ISR_B);
+    encoder4.setUpInterrupts(encoder4_jENCODER_ISR_A, encoder4_jENCODER_ISR_B);
+    encoder5.setUpInterrupts(encoder5_jENCODER_ISR_A, encoder5_jENCODER_ISR_B);
+    encoder6.setUpInterrupts(encoder6_jENCODER_ISR_A, encoder6_jENCODER_ISR_B);
+    encoder7.setUpInterrupts(encoder7_jENCODER_ISR_A, encoder7_jENCODER_ISR_B);
 }
 
 void Always()
 {
     // always runs if void loop is running, JMotor run() functions should be put here
     // (but only the "top level", for example if you call drivetrainController.run() you shouldn't also call leftMotorController.run())
+
+    // read velocity from all encoders (put in Always())
+    encoder2Val = encoder2.getVel();
+    encoder3Val = encoder3.getVel();
+    encoder4Val = encoder4.getVel();
+    encoder5Val = encoder5.getVel();
+    encoder6Val = encoder6.getVel();
+    encoder7Val = encoder7.getVel();
 
     delay(1);
 }
@@ -72,13 +175,31 @@ void WifiDataToParse()
     enabled = EWD::recvBl();
     // add data to read here: (EWD::recvBl, EWD::recvBy, EWD::recvIn, EWD::recvFl)(boolean, byte, int, float)
     // receive values for all the variables (you can put this in WifiDataToParse())
+    servo1Val = EWD::recvFl();
+    servo2Val = EWD::recvFl();
+    servo3Val = EWD::recvFl();
+    servo4Val = EWD::recvFl();
 
+    motor1Val = EWD::recvFl();
+    motor2Val = EWD::recvFl();
+    motor3Val = EWD::recvFl();
+    motor4Val = EWD::recvFl();
+    motor5Val = EWD::recvFl();
+    motor6Val = EWD::recvFl();
+    motor7Val = EWD::recvFl();
+    motor8Val = EWD::recvFl();
 }
 void WifiDataToSend()
 {
     EWD::sendFl(voltageComp.getSupplyVoltage());
     // add data to send here: (EWD::sendBl(), EWD::sendBy(), EWD::sendIn(), EWD::sendFl())(boolean, byte, int, float)
-
+    // send values for all the encoders (you can put this in WifiDataToSend())
+    EWD::sendFl(encoder2Val);
+    EWD::sendFl(encoder3Val);
+    EWD::sendFl(encoder4Val);
+    EWD::sendFl(encoder5Val);
+    EWD::sendFl(encoder6Val);
+    EWD::sendFl(encoder7Val);
 }
 
 void configWifi()
